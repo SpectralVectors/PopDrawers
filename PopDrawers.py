6aee6175d1f60642da018bb2cd761f12a7b4a7b5
SpectralVectors <robmuir1@gmail.com>
2021-11-03 14:07:28 -0400

    Responsive labels
    Buttons now indicate which area is open by switching its label to 'Close [x]' while its open

--- PopDrawers.py
+++ PopDrawers.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Pop Drawers",
     "description": "Open Temporary Areas in your Main Screen",
     "author": "Spectral Vectors",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (3, 00, 0),
     "location": "Text Editor",
     "category": "Text Editor"}
@@ -10,6 +10,10 @@ bl_info = {
 import bpy
 
 #bpy.types.Scene.popup_type = bpy.props.StringProperty(default='INFO')
+bpy.types.Scene.infoopened = bpy.props.BoolProperty(default=False)
+bpy.types.Scene.consoleopened = bpy.props.BoolProperty(default=False)
+bpy.types.Scene.propertiesopened = bpy.props.BoolProperty(default=False)
+bpy.types.Scene.outlineropened = bpy.props.BoolProperty(default=False)
 
 class InfoDrawerOperator(bpy.types.Operator):
     """Tooltip"""
@@ -26,17 +30,23 @@ class InfoDrawerOperator(bpy.types.Operator):
         current_type = bpy.context.space_data.type
         if bpy.context.scene.opened:
             bpy.context.screen.areas[-1].type = popup_type
+            bpy.context.scene.consoleopened = False
+            bpy.context.scene.propertiesopened = False
+            bpy.context.scene.outlineropened = False
+            bpy.context.scene.infoopened = True
         else:    
             bpy.ops.screen.space_type_set_or_cycle(space_type=popup_type)
             bpy.context.space_data.show_region_header = False
             bpy.ops.screen.area_split(direction=self.direction, factor=self.factor, cursor=(0,0))
             bpy.ops.screen.space_type_set_or_cycle(space_type=current_type)
             bpy.context.scene.opened = True
+            bpy.context.scene.infoopened = True
     
     @staticmethod    
     def close_drawer():
         bpy.ops.screen.area_close({"area": bpy.context.screen.areas[-1]})
-        bpy.context.scene.opened = False                
+        bpy.context.scene.opened = False
+        bpy.context.scene.infoopened = False
         
     def execute(self, context):
         if bpy.context.scene.opened and bpy.context.screen.areas[-1].type == self.popup_type:
@@ -62,17 +72,23 @@ class ConsoleDrawerOperator(bpy.types.Operator):
         current_type = bpy.context.space_data.type
         if bpy.context.scene.opened:
             bpy.context.screen.areas[-1].type = popup_type
+            bpy.context.scene.infoopened = False
+            bpy.context.scene.propertiesopened = False
+            bpy.context.scene.outlineropened = False
+            bpy.context.scene.consoleopened = True
         else:    
             bpy.ops.screen.space_type_set_or_cycle(space_type=popup_type)
             bpy.context.space_data.show_region_header = False
             bpy.ops.screen.area_split(direction=self.direction, factor=self.factor, cursor=(0,0))
             bpy.ops.screen.space_type_set_or_cycle(space_type=current_type)
             bpy.context.scene.opened = True
+            bpy.context.scene.consoleopened = True
     
     @staticmethod    
     def close_drawer():
         bpy.ops.screen.area_close({"area": bpy.context.screen.areas[-1]})
-        bpy.context.scene.opened = False                
+        bpy.context.scene.opened = False
+        bpy.context.scene.consoleopened = False                
         
     def execute(self, context):
         if bpy.context.scene.opened and bpy.context.screen.areas[-1].type == self.popup_type:
@@ -98,17 +114,23 @@ class PropertiesDrawerOperator(bpy.types.Operator):
         current_type = bpy.context.space_data.type
         if bpy.context.scene.opened:
             bpy.context.screen.areas[-1].type = popup_type
+            bpy.context.scene.consoleopened = False
+            bpy.context.scene.infoopened = False
+            bpy.context.scene.outlineropened = False
+            bpy.context.scene.propertiesopened = True
         else:    
             bpy.ops.screen.space_type_set_or_cycle(space_type=popup_type)
             bpy.context.space_data.show_region_header = False
             bpy.ops.screen.area_split(direction=self.direction, factor=self.factor, cursor=(0,0))
             bpy.ops.screen.space_type_set_or_cycle(space_type=current_type)
             bpy.context.scene.opened = True
+            bpy.context.scene.propertiesopened = True
     
     @staticmethod    
     def close_drawer():
         bpy.ops.screen.area_close({"area": bpy.context.screen.areas[-1]})
-        bpy.context.scene.opened = False                
+        bpy.context.scene.opened = False
+        bpy.context.scene.propertiesopened = False                
         
     def execute(self, context):
         if bpy.context.scene.opened and bpy.context.screen.areas[-1].type == self.popup_type:
@@ -134,17 +156,23 @@ class OutlinerDrawerOperator(bpy.types.Operator):
         current_type = bpy.context.space_data.type
         if bpy.context.scene.opened:
             bpy.context.screen.areas[-1].type = popup_type
+            bpy.context.scene.consoleopened = False
+            bpy.context.scene.propertiesopened = False
+            bpy.context.scene.infoopened = False
+            bpy.context.scene.outlineropened = True
         else:    
             bpy.ops.screen.space_type_set_or_cycle(space_type=popup_type)
             bpy.context.space_data.show_region_header = False
             bpy.ops.screen.area_split(direction=self.direction, factor=self.factor, cursor=(0,0))
             bpy.ops.screen.space_type_set_or_cycle(space_type=current_type)
             bpy.context.scene.opened = True
+            bpy.context.scene.outlineropened = True
     
     @staticmethod    
     def close_drawer():
         bpy.ops.screen.area_close({"area": bpy.context.screen.areas[-1]})
-        bpy.context.scene.opened = False                
+        bpy.context.scene.opened = False
+        bpy.context.scene.outlineropened = False                
         
     def execute(self, context):
         if bpy.context.scene.opened and bpy.context.screen.areas[-1].type == self.popup_type:
@@ -166,11 +194,28 @@ class PopDrawersPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
+        if bpy.context.scene.infoopened:
+            infolabel = 'Close [x]'
+        else:
+            infolabel = 'Info' 
+        if bpy.context.scene.consoleopened:
+            consolelabel = 'Close [x]'
+        else:
+            consolelabel = 'Console'        
+        if bpy.context.scene.propertiesopened:
+            propertieslabel = 'Close [x]'
+        else:
+            propertieslabel = 'Properties'
+        if bpy.context.scene.outlineropened:
+            outlinerlabel = 'Close [x]'
+        else:
+            outlinerlabel = 'Outliner'
+
         column = layout.column(align=True)            
-        column.operator(text='Info', operator="screen.info_drawer")
-        column.operator(text='Console', operator="screen.console_drawer")
-        column.operator(text='Properties', operator="screen.properties_drawer")
-        column.operator(text='Outliner', operator="screen.outliner_drawer")
+        column.operator(text=infolabel, operator="screen.info_drawer")
+        column.operator(text=consolelabel, operator="screen.console_drawer")
+        column.operator(text=propertieslabel, operator="screen.properties_drawer")
+        column.operator(text=outlinerlabel, operator="screen.outliner_drawer")
 
 
 classes = [
